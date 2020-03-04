5107110c0e0a7e5a87c809e687e42d917b927989
Vaibhav Ajay Gupta <vaibhav.ajaygupta@gmail.com>
2020-03-04 21:19:24 +0530

    updated the template

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Triumph Classes ( Best Coaching Classes )' });
 });
+
+/* Get about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Triumph Classes ( Best Coaching Classes )' });
+});
+
+
+/* Get contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Triumph Classes ( Best Coaching Classes )' });
+});
+
+
 
 module.exports = router;
